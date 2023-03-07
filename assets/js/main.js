4ede05514b02e0935a9c36ba0f2d9ce8150ca885
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2023-03-07 23:39:10 +0545

    added sidenav and topnav bar to the site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
-    $("#navigation").load("../../component/nav.html");
+    $("#navigation").load("../../component/sideNav.html");
+});
+$(document).ready(function() {
+    $("#content").css("display", "none");
+ 
+    $("#content").fadeIn(2000);
+ 
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("#content").fadeOut(1000, redirectPage);      
+    });
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
 });
