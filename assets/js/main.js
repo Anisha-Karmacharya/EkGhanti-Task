4f4056b78476e21438208ddc19e35c343003c267
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2023-03-10 12:02:51 +0545

    added form in the ticket page

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
   }
 });
 
+
 // #CHART
 // set the dimensions and margins of the graph
 var width = 450;
@@ -335,30 +336,3 @@ svg3
   .attr("text-anchor", "left")
   .style("alignment-baseline", "middle");
 
-
-
-  const links = document.querySelectorAll('a.nav__link');
-    console.log(links)
-  if (links.length) {
-    links.forEach((link) => {
-      link.addEventListener('click', (e) => {
-        links.forEach((link) => {
-            link.classList.remove('active');
-        });
-        e.preventDefault();
-        link.classList.add('active');
-      });
-    });
-  }
-
-
-
-  var header = document.getElementById("navList");
-  var btns = header.getElementsByClassName("nav__link");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    });
-  }
