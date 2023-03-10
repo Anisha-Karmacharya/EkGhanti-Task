cbcc2e0d156f25cffa0d0af0494e734100845df1
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2023-03-10 13:26:45 +0545

    added pagination to the table

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,29 @@ $(document).ready(function () {
   }
 });
 
+$(document).ready(function(){
+  $('#ticketTable').after('<div id="pageNav"></div>');
+  var visibleRows = 5;
+  var totalRows = $('#ticketTable tbody tr').length;
+  var numPages = totalRows/visibleRows;
+  for(i = 0;i < numPages;i++) {
+      var pageNum = i + 1;
+      $('#pageNav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+  }
+  $('#ticketTable tbody tr').hide();
+  $('#ticketTable tbody tr').slice(0, visibleRows).show();
+  $('#pageNav a:first').addClass('activePage');
+  $('#pageNav a').bind('click', function(){
+
+      $('#pageNav a').removeClass('activePage');
+      $(this).addClass('activePage');
+      var currPage = $(this).attr('rel');
+      var startItem = currPage * visibleRows;
+      var endItem = startItem + visibleRows;
+      $('#ticketTable tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+      css('display','table-row').animate({opacity:1}, 300);
+  });
+});
 
 // #CHART
 // set the dimensions and margins of the graph
